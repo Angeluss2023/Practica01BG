e2a596bd21a5db00b30a8ec414a877dea2a183eb
Angeluss2023 <121591004+Angeluss2023@users.noreply.github.com>
2023-04-18 18:39:39 -0500

    3 files modified

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ const contactForm = document.getElementById('contact-form');
 const formResult = document.getElementById('form-result');
 
 contactForm.addEventListener('submit');
+$(document).ready(function () {
+  // Agregar animación de desplazamiento suave a la barra de navegación
+  $('.navbar-link').click(function (event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 100,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
